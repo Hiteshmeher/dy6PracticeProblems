13dede884ea1fa3a6a41a91056f57632815c2ac2
Hiteshmeher <meher.hitesh4@gmail.com>
2022-07-25 09:19:23 +0530

    [Hitesh]: added day 6 practice problems

--- src/com/bridgelabz/day6/StopWatch.java
+++ src/com/bridgelabz/day6/StopWatch.java
@@ -13,18 +13,20 @@ public class StopWatch {
 		 * 
 		 */
 		long elapsed = 0;
-		long StartTime = System.currentTimeMillis();
+		long StartTime = 0;
+		long StopTime=0;
 		Scanner input = new Scanner(System.in);
 		System.out.println("Press 1 to start the stop watch ");
 		int N = input.nextInt();
 		if (N == 1) {
+			StartTime = System.currentTimeMillis();
 			System.out.println("Starting time is " + StartTime);
 		}
-		long StopTime = System.currentTimeMillis();
 		Scanner input2 = new Scanner(System.in);
 		System.out.println("Press 2 to start the stop watch ");
 		int M = input2.nextInt();
 		if (M == 2) {
+			StopTime = System.currentTimeMillis();
 			System.out.println("Starting time is " + StopTime);
 		}
 		elapsed = StopTime - StartTime;
